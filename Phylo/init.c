7e287a7b4c881354232b5f3954f66c97cae323d4
riko67 <109071422+riko67@users.noreply.github.com>
2023-09-09 18:07:09 +0200

    Add files via upload

---
+++ Phylo/init.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddelacou <ddelacou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/23 20:13:24 by ddelacou          #+#    #+#             */
+/*   Updated: 2023/09/09 17:21:11 by ddelacou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+#include <stdio.h>
+
+int init_mutex(t_global *global)
+{
+    int i;
+    i = 0;
+    global->fork_mutex = malloc(global->nbrephilo * sizeof(pthread_mutex_t));
+    if (!global->fork_mutex)
+    {
+        printf("Erreur d'allocation de mémoire.\n");
+        return (-1);
+    }
+    while (i < global->nbrephilo)
+    {
+        pthread_mutex_init(&global->fork_mutex[i], NULL);
+        i++;
+    }
+    return (0);
+}
+
+int init_philo(t_global *global, int *philosophers_done)
+{
+    int i;
+
+    i = 0;
+    global->philo = malloc(sizeof(t_philo) * global->nbrephilo);
+    if(!global->philo || !global->fork_mutex)
+        return (-1);
+    while (i < global->nbrephilo)
+    {
+        global->philo[i].global = global;
+        global->philo[i].id = i + 1;
+        global->philo[i].program_should_stop = &philosophers_done[i];
+        global->philo[i].time_of_last_meal.tv_sec = 0;
+        global->philo[i].time_of_last_meal.tv_usec = 0;
+        global->philo[i].meals_to_eat = global->meals_to_eat;
+        global->philo[i].fork_left = &(global->fork_mutex[i]);
+        if(global->nbrephilo != 1)
+            global->philo[i].fork_right = &(global->fork_mutex[(i + 1) % global->nbrephilo]);
+        i++;
+    }
+    return(0);
+}
+
+int *init_global(char **argv, t_global *global,int *philosophers_done)
+{
+    int i;
+
+    i = 0;
+    global->nbrephilo = atoi(argv[1]);
+    philosophers_done = malloc(global->nbrephilo * sizeof(int));
+    while(i < global->nbrephilo)
+    {
+        philosophers_done[i] = 0;
+        i++;
+    }
+    global->time_to_die = atoi(argv[2]);
+    global->time_to_eat = atoi(argv[3]);
+    global->time_to_sleep = atoi(argv[4]);
+    global->meals_to_eat = -1;
+    global->program_should_stop = 0;
+    if (argv[5])
+        global->meals_to_eat = atoi(argv[5]);
+    gettimeofday(&global->program_start, NULL);
+    global->program_should_stop = 0;
+    return (philosophers_done);
+} 
+
+int *init_all(t_global *global, char **argv, int *philosophers_done, pthread_t *threads)
+{
+    philosophers_done = init_global(argv, global, philosophers_done);
+    init_mutex(global);
+    init_philo(global, philosophers_done);
+    init_threads(global, threads);
+    return(philosophers_done);
+}
+
+int parser_arg(char **argv, int argc)
+{
+    int i;
+    int j;
+
+    j = 0;
+    i = 1;
+    if (argc < 4 || argc > 5)
+        return(-1);
+    while (i != argc - 1)
+    {
+        while(argv[i][j])
+        {
+            if((argv[i][j] > 'a' && argv[i][j] < 'z') || (argv[i][j] > 'A' && argv[i][j] < 'Z'))
+                return(-1);
+            j++;
+        }   
+        i++;
+    }
+    return(1);
+}
---
+++ Phylo/philo2.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddelacou <ddelacou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/28 19:11:00 by ddelacou          #+#    #+#             */
+/*   Updated: 2023/09/09 17:52:10 by ddelacou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+#include <stdio.h>
+
+int get_time_since_start(struct timeval program_start)
+{
+    struct timeval current_time;
+    gettimeofday(&current_time, NULL);
+    return (current_time.tv_sec - program_start.tv_sec) * 1000 +
+           (current_time.tv_usec - program_start.tv_usec) / 1000;
+}
+void *philosopher(void *arg) 
+{
+    t_philo *philo;
+    int id;
+    int meals_eaten;
+    int temps_attente;
+    struct timeval now;
+
+    philo = (t_philo *)arg;
+    id = philo->id;
+    meals_eaten = 0;
+
+    while (1)
+    {
+        printf("%d %d is thinking\n", get_time_since_start(philo->global->program_start), id);
+        pthread_mutex_lock(philo->fork_left);
+        printf("%d %d has taken a fork\n", get_time_since_start(philo->global->program_start), id);
+        pthread_mutex_lock(philo->fork_right);
+        printf("%d %d has taken a fork\n", get_time_since_start(philo->global->program_start), id);
+        gettimeofday(&now, NULL);
+        temps_attente = ((now.tv_sec - philo->time_of_last_meal.tv_sec) * 1000 + (now.tv_usec - philo->time_of_last_meal.tv_usec) / 1000);
+        if(temps_attente >= philo->time_to_die)
+        {
+            printf("%d %d died\n", get_time_since_start(philo->global->program_start), id);
+            *(philo->program_should_stop) = 1;
+            philo->global->program_should_stop = 1;
+            break;
+        }
+        meals_eaten++;
+        printf("%d %d is eating\n", get_time_since_start(philo->global->program_start), id);
+        usleep(philo->time_to_eat * 1000);
+        gettimeofday(&philo->time_of_last_meal, NULL);
+        pthread_mutex_unlock(philo->fork_left);
+        pthread_mutex_unlock(philo->fork_right);
+        if (philo->meals_to_eat != -1 && meals_eaten >= philo->meals_to_eat) 
+        {
+            *(philo->program_should_stop) = 2;
+            break;
+        }
+        printf("%d %d is sleeping\n", get_time_since_start(philo->global->program_start), id);
+        usleep(philo->time_to_sleep * 1000);
+    }
+    return (NULL);
+}
+
+void free_all(t_global *global)
+{
+    int i;
+    
+    i = 0;
+    free(global->philo);
+    while(i < global->nbrephilo)
+    {
+        pthread_mutex_destroy(&global->fork_mutex[i]);
+        i++;
+    }
+    free(global->fork_mutex);
+}
+
+void init_threads(t_global *global, pthread_t *threads)
+{
+    int i;
+    
+    i = 0;
+    while (i < global->nbrephilo)
+    {
+        pthread_create(&threads[i], NULL, philosopher, &global->philo[i]);
+        pthread_detach(threads[i]);
+        i++;
+    }
+}
